53f8f32224796e6177a920586b6c21dbe042ab93
Kutlualp Tazefidan <ktazefidan@hotmail.com>
2023-09-16 08:18:34 +0200

    Include new schema to connect to the api

--- src/components/CalculateButton.js
+++ src/components/CalculateButton.js
@@ -6,6 +6,7 @@ import { useState } from "react";
 import { storage } from "../storage/storage";
 import { useSnapshot } from "valtio";
 import { getDistance } from "geolib";
+var airlineCodeTranslator = require("airline-codes");
 
 export default function CalculateButton({ airportsData }) {
   const {
@@ -22,105 +23,114 @@ export default function CalculateButton({ airportsData }) {
   const [estDelay, setEstDelay] = useState(0);
 
   async function fetchData() {
-    console.log("Fetching data!");
-    console.log(
-      `Departure Airport (Type: ${typeof departure_airport}): ${departure_airport}\n` +
-        `Departure City (Type: ${typeof departure_city}): ${departure_city}\n` +
-        `Departure Country (Type: ${typeof departure_country}): ${departure_country}\n` +
-        `Departure Time (Type: ${typeof departure_time}): ${departure_time}\n` +
-        `Arrival Airport (Type: ${typeof arrival_airport}): ${arrival_airport}\n` +
-        `Arrival City (Type: ${typeof arrival_city}): ${arrival_city}\n` +
-        `Arrival Country (Type: ${typeof arrival_country}): ${arrival_country}\n` +
-        `Arrival Time (Type: ${typeof arrival_time}): ${arrival_time}\n` +
-        `Airline (Type: ${typeof airline}): ${airline}`
-    );
-    // Formating  data for api
-
-    // date
-    // Format the date to 'YYYY-MM-DD HH:mm:ss'
-    const departure_time_f = departure_time
-      .toISOString()
-      .replace(/T/, " ")
-      .replace(/\..+/, "");
-    const arrival_time_f = arrival_time
-      .toISOString()
-      .replace(/T/, " ")
-      .replace(/\..+/, "");
-
-    const year = departure_time.getFullYear();
-    const month = (departure_time.getMonth() + 1).toString().padStart(2, "0"); // Month is zero-based, so we add 1
-    const day = departure_time.getDate().toString().padStart(2, "0");
-    const datop = `${year}${month}${day}`;
-
-    const dhour = departure_time.getHours().toString().padStart(2, "0");
-    const dminute = departure_time.getMinutes().toString().padStart(2, "0");
-    const dsecond = departure_time.getSeconds().toString().padStart(2, "0");
-    const std_time = `${dhour}${dminute}${dsecond}`;
-
-    const ahour = departure_time.getHours().toString().padStart(2, "0");
-    const aminute = departure_time.getMinutes().toString().padStart(2, "0");
-    const asecond = departure_time.getSeconds().toString().padStart(2, "0");
-    const sta_time = `${ahour}${aminute}${asecond}`;
-
-    // Determine the season based on the month and assign a numerical value
-    let season = 0;
-    if (month >= 3 && month <= 5) {
-      season = 0; // Spring
-    } else if (month >= 6 && month <= 8) {
-      season = 1; // Summer
-    } else if (month >= 9 && month <= 11) {
-      season = 2; // Autumn
-    } else {
-      season = 3; // Winter
-    }
+    console.log("Sending Data to the API!");
 
     const departure_airport_data = airportsData[departure_airport];
     const arrival_airport_data = airportsData[arrival_airport];
-    console.log(airportsData[departure_airport]);
-    const departure_lon = departure_airport_data["lon"];
-    const departure_lat = departure_airport_data["lat"];
-    const departure_elevation = departure_airport_data["elevation"];
-    const arrival_lon = arrival_airport_data["lon"];
-    const arrival_lat = arrival_airport_data["lat"];
-    const arrival_elevation = arrival_airport_data["elevation"];
-    const elevation_difference = departure_elevation - arrival_elevation;
+    // Formating  data for api
+    const id = "w_id_0";
+    const datop = departure_time.toISOString().split("T")[0];
+    const fltid = airlineCodeTranslator
+      .findWhere({ name: airline })
+      .get("icao"); // abbriviation needed
+    const depstn = departure_airport_data["iata"];
+    const arrstn = arrival_airport_data["iata"];
+
+    // date
+    const std = departure_time
+      .toISOString()
+      .replace(/T/, " ")
+      .replace(/\..+/, "");
+    const sta = arrival_time
+      .toISOString()
+      .replace(/T/, " ")
+      .replace(/\..+/, "");
+
+    const status = "ATA";
+    const ac = airlineCodeTranslator.findWhere({ name: airline }).get("icao");
+    const dep_iata = departure_airport_data["iata"];
+    const dep_country = departure_airport_data["country"];
+    const dep_elevation = departure_airport_data["elevation"];
+    const dep_lat = departure_airport_data["lat"];
+    const dep_lon = departure_airport_data["lon"];
+    const arr_iata = arrival_airport_data["iata"];
+    const arr_country = arrival_airport_data["country"];
+    const arr_elevation = arrival_airport_data["elevation"];
+    const arr_lat = arrival_airport_data["lat"];
+    const arr_lon = arrival_airport_data["lon"];
+
     console.log(
-      getDistance(
-        { latitude: departure_lat, longitude: departure_lon },
-        { latitude: arrival_lat, longitude: arrival_lon }
-      )
+      `id (Type: ${typeof id}): ${id}\n` +
+        `datop (Type: ${typeof datop}): ${datop}\n` +
+        `fltid (Type: ${typeof fltid}): ${fltid}\n` +
+        `depstn (Type: ${typeof depstn}): ${depstn}\n` +
+        `arrstn (Type: ${typeof arrstn}): ${arrstn}\n` +
+        `std (Type: ${typeof std}): ${std}\n` +
+        `sta (Type: ${typeof sta}): ${sta}\n` +
+        `status (Type: ${typeof status}): ${status}\n` +
+        `ac (Type: ${typeof ac}): ${ac}\n` +
+        `dep_iata (Type: ${typeof dep_iata}): ${dep_iata}\n` +
+        `dep_country (Type: ${typeof dep_country}): ${dep_country}\n` +
+        `dep_elevation (Type: ${typeof dep_elevation}): ${dep_elevation}\n` +
+        `dep_lat (Type: ${typeof dep_lat}): ${dep_lat}\n` +
+        `dep_lon (Type: ${typeof dep_lon}): ${dep_lon}\n` +
+        `arr_iata (Type: ${typeof arr_iata}): ${arr_iata}\n` +
+        `arr_country (Type: ${typeof arr_country}): ${arr_country}\n` +
+        `arr_elevation (Type: ${typeof arr_elevation}): ${arr_elevation}\n` +
+        `arr_lat (Type: ${typeof arr_lat}): ${arr_lat}\n` +
+        `arr_lon (Type: ${typeof arr_lon}): ${arr_lon}`
     );
-    // try {
-    //   const response = await fetch("https://flight-delay-api.onrender.com", {
-    //     method: "POST",
-    //     headers: {
-    //       "Content-Type": "application/json",
-    //     },
-    //     body: JSON.stringify({
-    //       departure_airport: str,
-    //       departure_city: str,
-    //       departure_country: str,
-    //       departure_time: str,
-    //       arrival_airport: str,
-    //       arrival_city: str,
-    //       arrival_country: str,
-    //       arrival_time: str,
-    //       airline: str,
-    //     }),
-    //   });
 
-    //   if (!response.ok) {
-    //     throw new Error("Network response was not ok");
-    //   }
+    console.log(
+      "trying",
+      airlineCodeTranslator.findWhere({ name: airline }).get("icao")
+    );
 
-    //   const data = await response.json();
-    //   setEstDelay(data["prediction"]);
-    //   // Use the data in your Next.js app
-    //   console.log(data);
-    //   console.log(data["prediction"]);
-    // } catch (error) {
-    //   console.error("There was a problem with the fetch operation:", error);
-    // }
+    try {
+      const response = await fetch(
+        // "https://flight-delay-api.onrender.com/preprocessdata",
+        "http://localhost:10000/preprocessdata",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            id: id,
+            datop: datop,
+            fltid: fltid,
+            depstn: depstn,
+            arrstn: arrstn,
+            std: std,
+            sta: sta,
+            status: status,
+            ac: ac,
+            dep_iata: dep_iata,
+            dep_country: dep_country,
+            dep_elevation: dep_elevation,
+            dep_lat: dep_lat,
+            dep_lon: dep_lon,
+            arr_iata: arr_iata,
+            arr_country: arr_country,
+            arr_elevation: arr_elevation,
+            arr_lat: arr_lat,
+            arr_lon: arr_lon,
+          }),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      const data = await response.json();
+      // setEstDelay(data["prediction"]);
+      // Use the data in your Next.js app
+      console.log(data);
+      // console.log(data["prediction"]);
+    } catch (error) {
+      console.error("There was a problem with the fetch operation:", error);
+    }
   }
 
   return (
